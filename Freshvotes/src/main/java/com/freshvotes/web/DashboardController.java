49d813757a23afd324faba7396dbd810f35ff74a
Igor_Timbalenco <igor.timbalenco@gmail.com>
2020-03-18 19:17:59 +0200

    Adding password encoder

--- Freshvotes/src/main/java/com/freshvotes/web/DashboardController.java
+++ Freshvotes/src/main/java/com/freshvotes/web/DashboardController.java
@@ -1,13 +1,17 @@
-package com.freshvotes;
+package com.freshvotes.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard";
+	}
+	
+	@GetMapping("/")
 	public String rootView() {
 		return "index";
 	}
